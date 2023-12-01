d8853b433b296a17dc552f1b708110e26852255a
lakpa-finju <connectedtofinju1@gmail.com>
2023-11-30 21:25:34 -0500

    implemented to stringa and equals for addition class

--- src/picasso/parser/language/expressions/Addition.java
+++ src/picasso/parser/language/expressions/Addition.java
@@ -2,18 +2,18 @@ package picasso.parser.language.expressions;
 
 import picasso.parser.language.ExpressionTreeNode;
 
-public class Addition extends ExpressionTreeNode{
+public class Addition extends ExpressionTreeNode {
 	ExpressionTreeNode left;
 	ExpressionTreeNode right;
 
-	public Addition(ExpressionTreeNode left, ExpressionTreeNode right ) {
+	public Addition(ExpressionTreeNode left, ExpressionTreeNode right) {
 		this.left = left;
 		this.right = right;
 	}
 
 	/**
-	 * Evaluates this expression at the given x,y point by evaluating the addition of
-	 * the function's parameter.
+	 * Evaluates this expression at the given x,y point by evaluating the addition
+	 * of the function's parameter.
 	 * 
 	 * @return the color from evaluating the addition of the expression's parameter
 	 */
@@ -27,5 +27,41 @@ public class Addition extends ExpressionTreeNode{
 
 		return new RGBColor(red, green, blue);
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+
+		if (!(o instanceof Addition)) {
+			return false;
+		}
+
+		// Make sure the objects are the same type
+		if (o.getClass() != this.getClass()) {
+			return false;
+		}
+
+		Addition other = (Addition) o;
+		if (!other.left.equals(this.left)) {
+			return false;
+		}
+
+		if (!other.right.equals(this.right)) {
+			return false;
+		}
+
+		return true;
+
+	}
+	@Override
+    public String toString(){
+        StringBuilder str = new StringBuilder(""); 
+        str.append(left); 
+        str.append(" + "); 
+        str.append(right);
+        return str.toString(); 
+    }
 
 }
