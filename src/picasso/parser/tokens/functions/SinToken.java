aca7bcf78f066492ebaf1fb0276a734a1f8a349b
Liz Kent <kentl22@mail.wlu.edu>
2023-11-30 20:35:59 -0500

    Adding classes for Sine function.

---
+++ src/picasso/parser/tokens/functions/SinToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the Sine function token
+ * 
+ */
+public class SinToken extends FunctionToken {
+
+	public SinToken() {
+		super("Sin Function Token");
+	}
+
+}
+
