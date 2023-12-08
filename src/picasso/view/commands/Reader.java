7b8ab1c0edb48d223f67071255a267717b72257e
TreyCustodio <tcustodio@mail.wlu.edu>
2023-12-07 23:15:58 -0500

    Error handling for invalid image types

--- src/picasso/view/commands/Reader.java
+++ src/picasso/view/commands/Reader.java
@@ -11,7 +11,9 @@ import picasso.util.FileCommand;
  * @author Robert C Duvall
  */
 public class Reader extends FileCommand<Pixmap> {
-
+	
+	protected static String[] extensions = {".png", ".jpg", ".gif"};
+	
 	/**
 	 * Creates a Reader object, which prompts users for image files to open
 	 */
@@ -23,9 +25,26 @@ public class Reader extends FileCommand<Pixmap> {
 	 * Displays the image file on the given target.
 	 */
 	public void execute(Pixmap target) {
+		//Called when you press open image or open expression
+		System.out.println("Open file");
 		String fileName = getFileName();
+		System.out.println(fileName);
 		if (fileName != null) {
-			target.read(fileName);
+			System.out.println("target.read");
+			String suffix = fileName.substring(fileName.lastIndexOf("."));
+			if (suffix.equals(".png") || suffix.equals(".jpg")) {
+				target.read(fileName);
+			}
+			else {
+				System.out.println("error");
+				String errorFile = System.getProperty("user.dir") +
+						"\\images\\Error_Image.png";
+				
+				target.read(errorFile);
+			}
 		}
+		
+		
+		
 	}
 }
