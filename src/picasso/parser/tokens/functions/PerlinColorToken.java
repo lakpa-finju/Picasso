f319ca0482ad276a18bc51312353b7e65f94d75b
Linh Nguyen <nguyenl25@mail.wlu.edu>
2023-12-09 16:08:39 -0500

    implement PerlinColor. Trey's error-handling pop up -- need to fix after asking Trey

---
+++ src/picasso/parser/tokens/functions/PerlinColorToken.java
@@ -0,0 +1,12 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the perlinColor function token
+ * 
+ */
+public class PerlinColorToken extends FunctionToken {
+
+	public PerlinColorToken() {
+		super("PerlinColorToken Function Token");
+	}
+}
