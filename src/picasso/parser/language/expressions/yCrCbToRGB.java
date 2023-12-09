d49fb4e5ba02ce49bb32c76cdf15a68f402b6930
Linh Nguyen <nguyenl25@mail.wlu.edu>
2023-12-08 22:50:08 -0500

    Implement YCrCbToRGB and RgbToYCrCb function

---
+++ src/picasso/parser/language/expressions/yCrCbToRGB.java
@@ -0,0 +1,38 @@
+package picasso.parser.language.expressions;
+
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the abs function in the Picasso language.
+ * 
+ * @author Linh Nguyen
+ *
+ * 
+ */
+public class yCrCbToRGB extends UnaryFunction {
+
+	/**
+	 * Create a floor expression that takes as a parameter the given expression
+	 * 
+	 * @param param the expression to floor
+	 */
+	public yCrCbToRGB(ExpressionTreeNode param) {
+		super(param);
+	}
+
+	/**
+	 * Evaluates this expression at the given x,y point by evaluating the floor of
+	 * the function's parameter.
+	 * 
+	 * @return the color from evaluating the floor of the expression's parameter
+	 */
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = result.getRed() + result.getBlue() * 1.4022;
+		double green = result.getRed() + result.getGreen() * -0.3456 + result.getBlue() * -0.7145;
+		double blue = result.getRed() + result.getGreen() * 1.7710;
+
+		return new RGBColor(red, green, blue);
+	}
+}
