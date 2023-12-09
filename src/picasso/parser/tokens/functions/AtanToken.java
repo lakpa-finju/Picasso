dc5f4f82bc217ddffc4fe3626a87c064a3506570
Liz Kent <kentl22@mail.wlu.edu>
2023-12-09 18:21:15 -0500

    Atan implemented

---
+++ src/picasso/parser/tokens/functions/AtanToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the Atan function token
+ * 
+ */
+public class AtanToken extends FunctionToken {
+
+	public AtanToken() {
+		super("Atan Function Token");
+	}
+
+}
+
