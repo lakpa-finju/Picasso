af78cfffb07655af83979861c1070c986b0015b1
Linh Nguyen <nguyenl25@mail.wlu.edu>
2023-12-13 16:46:12 -0500

    x,y cannot be assignment variable -- implemented

--- src/picasso/parser/AssignmentAnalyzer.java
+++ src/picasso/parser/AssignmentAnalyzer.java
@@ -1,5 +1,6 @@
 package picasso.parser;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
@@ -18,17 +19,25 @@ public class AssignmentAnalyzer implements SemanticAnalyzerInterface {
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
 		tokens.pop(); // Remove the assignment token
-		// left side of the assignment token is the expression 
+		// right side of the assignment token is the expression 
 		ExpressionTreeNode rightExpression = SemanticAnalyzer.getInstance().generateExpressionTree(tokens);
-		IdentifierToken idToken = (IdentifierToken) tokens.pop(); 
-		String nameidToken = idToken.getName(); 
-		System.out.println("Still variable"); 
-		if (nameidToken == "x" ||nameidToken == "y" || nameidToken == "X" || nameidToken == "Y") {
-			throw new ParseException("Variable name cannot be x or y"); 
+		try {
+			if (tokens.peek() instanceof IdentifierToken) {
+
+				IdentifierToken idToken = (IdentifierToken) tokens.pop();
+				String nameidToken = idToken.getName();
+				if (nameidToken.equals("x") || nameidToken.equals("y") || nameidToken.equals("X") || nameidToken.equals("Y")){
+					throw new ParseException("Variable name cannot be x or y"); 
+				}
+				Variable idVariable = new Variable(nameidToken);
+				return new Assignment(idVariable, rightExpression);
+			} 
+			else {
+				throw new ParseException("Unable to perform assignment on the lefthand expression");
+			}
+		} catch (EmptyStackException e) {
+			throw new ParseException("Incomplete expression");
 		}
-		System.out.println("run after exception"); 
-		Variable idVariable = new Variable(nameidToken); 
-		return new Assignment(idVariable, rightExpression); 
 	}
 }
 
